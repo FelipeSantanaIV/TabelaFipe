52d8f2d9a43b2271c95b84b9f71d9b2ddfde3c7f
Felipe SMS <felipe.santana1995@hotmail.com>
2024-03-04 12:00:22 -0300

    Criando a busca de todas as avaliações do veículo.

--- src/main/java/br/com/felipe/TabelaFipe/principal/Principal.java
+++ src/main/java/br/com/felipe/TabelaFipe/principal/Principal.java
@@ -2,11 +2,15 @@ package br.com.felipe.TabelaFipe.principal;
 
 import br.com.felipe.TabelaFipe.model.Dados;
 import br.com.felipe.TabelaFipe.model.Modelos;
+import br.com.felipe.TabelaFipe.model.Veiculo;
 import br.com.felipe.TabelaFipe.service.ConsumeAPI;
 import br.com.felipe.TabelaFipe.service.ConverteDados;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
 
@@ -57,5 +61,36 @@ public class Principal {
         modeloLista.modelos().stream()
                 .sorted(Comparator.comparing(Dados::codigo))
                 .forEach(System.out::println);
+
+        System.out.println("\nDigite um trecho do nome do veículo a ser buscado: ");
+        var nomeVeiculo = leitura.nextLine();
+
+        List<Dados> modelosFiltrados = modeloLista.modelos().stream()
+                .filter(m -> m.nome().toLowerCase().contains(nomeVeiculo.toLowerCase()))
+                .collect(Collectors.toList());
+
+        System.out.println("\nModelos filtrados");
+        modelosFiltrados.forEach(System.out::println);
+
+        System.out.println("Digite por favor o código do modelo para buscar os valores do veículo. ");
+        var codigoModelo = leitura.nextLine();
+
+        endereco = endereco + "/" + codigoModelo + "/anos";
+        json = consumeAPI.obterDados(endereco);
+        List<Dados> anos = conversor.obterLista(json, Dados.class);
+        List<Veiculo> veiculos = new ArrayList<>();
+
+        for (int i = 0; i < anos.size(); i++) {
+            var enderecoAnos = endereco + "/" + anos.get(i).codigo();
+            json = consumeAPI.obterDados(enderecoAnos);
+            Veiculo veiculo = conversor.obterDados(json,, Veiculo.class);
+            veiculos.add(veiculo);
+        }
+
+        System.out.println("\nTodos os veículos encontrado com avaliações por ano: ");
+        veiculos.forEach(System.out::println);
+
+
+
     }
 }
